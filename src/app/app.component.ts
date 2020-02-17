90dac8e5b86abffcc72249a469383e090a5c0471
chandrakala123 <31918111+chandrakala123@users.noreply.github.com>
2020-02-17 17:38:37 +0530

    testing changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'gtm';
+export class AppComponent implements OnInit {
+  // title = 'gtm';
 
   constructor(
   ) { }
